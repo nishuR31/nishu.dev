55f7b039e4ecb06214de3cfae2d10c1b8f2aaf11
Nishu <nishangithub@gmail.com>
2026-02-10 11:17:54 +0530

    Update portfolio metadata for Nishan Rajak

--- app/layout.js
+++ app/layout.js
@@ -15,13 +15,13 @@ const geistSans = localFont({
 });
 
 export const metadata = {
-  title: "Huzaif\'s Portfolio",
+  title: "Nishan\'s Portfolio",
   icons:{
 icon: "/logo.png"
   },
-  description: "Experienced fullstack developer specializing in modern web technologies. View my projects, skills, and experience.",
-  keywords: "fullstack developer,backend developer, frontend developer, web development, JavaScript,JS, C#, CSharp, React, Node.js, portfolio",
-  author: "Huzaif Ahmed",
+  description: "Experienced fullstack developer specializing in backend background. View my projects, skills, and experience.",
+  keywords: "fullstack developer,backend developer, web development, JavaScript,JS, C#, CSharp, React, Node.js, portfolio, Typescript, Ts",
+  author: "Nishan Rajak",
   robots: "index, follow",
 };
 
